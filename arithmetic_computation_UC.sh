33511d86dec72ad68d92d2cdaf96f306b49ac849
codexakshay <codexakshay@gmail.com>
2020-09-05 14:07:51 +0530

    UC_5_arithmetic_computation

--- arithmetic_computation_UC.sh
+++ arithmetic_computation_UC.sh
@@ -1,4 +1,18 @@
+declare -A dict
 read -p "ENTER A : " a
 read -p "ENTER B : " b
 read -p "ENTER C : " c
-echo "( a % b + c ) : $(($a%$b+$c))"
+c1=$(($a+$b*$c))
+c2=$(($a*$b+$c))
+c3=$(($c+$a/$b))
+c4=$(($a%$b+$c))
+echo -e "\n( a + b x c ) : $(($a+$b*$c))"
+echo -e "\n( a x b + c ) : $(($a*$b+$c))"
+echo -e "\n( c + a / b ) : $(($c+$a/$b))"
+echo -e "\n( a % b + c ) : $(($a%$b+$c))"
+dict["c1"]+=$c1
+dict["c2"]+=$c2
+dict["c3"]+=$c3
+dict["c4"]+=$c4
+
+echo -e "\nDICTIONARY : ${dict[@]}"
