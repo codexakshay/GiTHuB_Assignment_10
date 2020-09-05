3df2f0741482ca9a3465351a703e787eb8893ca9
codexakshay <codexakshay@gmail.com>
2020-09-05 14:14:08 +0530

    UC_8_arithmetic_computation

--- arithmetic_computation_UC.sh
+++ arithmetic_computation_UC.sh
@@ -1,21 +1,29 @@
 declare -A dict
 declare -a arr
+
 read -p "ENTER A : " a
 read -p "ENTER B : " b
 read -p "ENTER C : " c
+
 c1=$(($a+$b*$c))
 c2=$(($a*$b+$c))
 c3=$(($c+$a/$b))
 c4=$(($a%$b+$c))
+
 echo -e "\n( a + b x c ) : $(($a+$b*$c))"
 echo -e "\n( a x b + c ) : $(($a*$b+$c))"
 echo -e "\n( c + a / b ) : $(($c+$a/$b))"
 echo -e "\n( a % b + c ) : $(($a%$b+$c))"
+
 dict["c1"]+=$c1
 dict["c2"]+=$c2
 dict["c3"]+=$c3
 dict["c4"]+=$c4
+
 a=0
 arr[((a++))]=${dict[@]}
 echo -e "\nDICTIONARY : ${dict[@]}"
 echo -e "\nARRAY VALs : ${arr[@]}"
+
+s_arr_d=($(echo ${arr[@]}| tr " " "\n" | sort -r))
+echo -e "\nSORTED DES : ${s_arr_d[@]}" #MAX TO MIN
